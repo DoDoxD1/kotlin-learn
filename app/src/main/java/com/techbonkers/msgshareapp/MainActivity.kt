a46422e7cb8c272d1c65f72d4724b96820bb74a3
aunuj <aunujain53@gmail.com>
2023-10-26 18:07:48 +0530

    first

---
+++ app/src/main/java/com/techbonkers/msgshareapp/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.techbonkers.msgshareapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val showMsgBtn = findViewById<Button>(R.id.showMsgBtn);
+        showMsgBtn.setOnClickListener{
+            Toast.makeText(this,"hi",Toast.LENGTH_SHORT).show()
+//            Log.i("aunu","Button is Clicked")
+        }
+
+    }
+}
